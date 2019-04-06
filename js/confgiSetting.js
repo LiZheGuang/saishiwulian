640d2cc81799b1ad3372bc12409bc448d1cebccd
LiZheGuang <1030660726@qq.com>
2019-04-07 00:05:46 +0800

    修改swtich

--- js/confgiSetting.js
+++ js/confgiSetting.js
@@ -1,20 +1,30 @@
 var seeting = {
     // 开关
     switch: `<div class="grid-stack-item-content">
-                <div class="switchingBox">
-                    <div class="switchingBox_title">1#电动机</div>
-                    <div class="toggle-button-wrapper">
-                            <input type="checkbox" id="toggle-button" name="switch">
-                            <label for="toggle-button" class="button-label">
-                                <span class="circle"></span>
-                                <span class="text on">ON</span>
-                                <span class="text off">OFF</span>
-                            </label>
-                    </div>
-                    <div class="settingIcon"></div>
-                    <div class="settingminiIcon"></div>
-                </div>
-            </div>`
-            
+    <div class="switchingBox">
+        <div class="switchingBox_title">1#电动机</div>
+        <div id="switchDom "  data-type="yes" class="switchCode  switchCodeYes">
+            <div class="solidSwitch"></div>
+            <div class="switchCodeMsg ">NO</div>
+        </div>
+        <div class="switchIcon"></div>
+    </div>
+</div>`
+
 }
 
+
+function switchId(keyID) {
+    return `
+    <div class="grid-stack-item-content">
+    <div class="switchingBox">
+        <div class="switchingBox_title">1#电动机</div>
+        <div id="${keyID}" data-type="yes" class="switchCode  switchCodeYes">
+            <div class="solidSwitch"></div>
+            <div class="switchCodeMsg ">NO</div>
+        </div>
+        <div class="switchIcon"></div>
+    </div>
+</div>
+    `
+}
