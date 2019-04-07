0af6a020287262268f7c6f65117c40e3d9092f3e
LiZheGuang <1030660726@qq.com>
2019-04-08 00:48:16 +0800

    beifen

--- js/confgiSetting.js
+++ js/confgiSetting.js
@@ -5,33 +5,111 @@ var seeting = {
 
 // 开关
 function switchId(keyID) {
-    return `
-    <div class="grid-stack-item-content" id="${keyID}">
-    <div class="switchingBox clearfix">
-        <div class="switchingBox_title">1#电动机</div>
-        <div id="swidchJs" data-type="yes" class="switchCode  switchCodeYes">
-            <div class="solidSwitch"></div>
-            <div class="switchCodeMsg ">NO</div>
-        </div>
-        <div class="switchIcon"></div>
-        <div class="switchIconSetting">
-        <div class="switchIconSetting_tab">
-            <div class="switchIconSetting_tab_item" id="edit">编辑属性</div>
-            <div class="switchIconSetting_tab_item" id="copy">复制</div>
-            <div class="switchIconSetting_tab_item" id="suoding">锁定</div>
-            <div class="switchIconSetting_tab_item" id="delete">删除</div>
-        </div>
-        </div>
-    </div>
-</div>
-    `
+
+   return  `<div class="dragulaDomClass clearfix" id="${keyID}" >
+                <div class="switchingBox clearfix">
+                    <div class="switchingBox_title">1#电动机</div>
+                    <div id="swidchJs" data-type="yes" class="switchCode  switchCodeYes">
+                        <div class="solidSwitch"></div>
+                        <div class="switchCodeMsg ">NO</div>
+                    </div>
+                    <div class="switchIcon"></div>
+                    <div class="switchIconSetting">
+                        <!-- 设置 -->
+                        <div class="switchIconSetting_tab">
+                            <div class="switchIconSetting_tab_item" id="edit">编辑属性</div>
+                            <div class="switchIconSetting_tab_item" id="copy">复制</div>
+                            <div class="switchIconSetting_tab_item" id="suoding">锁定</div>
+                            <div class="switchIconSetting_tab_item" id="delete">删除</div>
+                        </div>
+                        <!-- 设置 -->
+                        <div class="switchIconSetting_options" id="switchIconSetting_optionsDom" hidden>
+                        <div class="switchIconSetting_options_nav">
+                            <!-- 组件选项开关模块 -->
+                            <div>组件选项开关模块</div>
+                            <div class="switchIconSetting_options_nav_cler">关闭</div>
+                        </div>
+    
+                        <div class="switchIconSetting_options_body">
+                            <div class="switchIconSetting_options_body_color layui-form">
+                                <p>颜色</p>
+                                <div id="switchIconSetting_options_body_color_elementId">
+                                    <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+                                        <legend>预定义颜色项</legend>
+                                    </fieldset>
+                                </div>
+                                <p>标题</p>
+                                <div class="layui-form-item">
+                                    <input type="text" name="title" required lay-verify="required" placeholder="请输入标题"
+                                        autocomplete="off" class="layui-input">
+                                </div>
+    
+                                <p>数据绑定</p>
+                                <div class=" layui-form-item">
+                                    <div class="layui-input-inline">
+                                        <select name="quiz2">
+                                            <option value="">请选择市</option>
+                                            <option value="杭州">杭州</option>
+                                            <option value="宁波" disabled="">宁波</option>
+                                            <option value="温州">温州</option>
+                                            <option value="温州">台州</option>
+                                            <option value="温州">绍兴</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <p>开关样式</p>
+                                <div class="layui-form-item">
+    
+                                    <input type="radio" name="sex" value="男" title="样式一" checked="">
+                                    <input type="radio" name="sex" value="女" title="样式二">
+                                    <input type="radio" name="sex" value="女" title="样式三">
+                                </div>
+    
+                                <p>图标</p>
+    
+                                <div class="switchIconSetting_options_icon">
+                                    <span class="iconColor">请选择数据点图标</span>
+                                    <div class="click_switchIconSetting_options_icon_box">
+                                        <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                            alt="选择图片">
+                                        <!-- 点击后的展示 -->
+                                        <div class="click_switchIconSetting_options_icon_show">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+                                            <img class="click_switchIconSetting_options_icon" src="./images/icon_wd.png"
+                                                alt="选择图片">
+    
+                                        </div>
+                                        <!-- 点击后的展示 -->
+    
+                                    </div>
+                                </div>
+    
+                            </div>
+                            <!-- 显示选项 -->
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+                `
 }
 
 // 温度传感器
 function temperature() {
     return `
-    <div class="grid-stack-item-content">
-        <div class="switchingBox">
+    <div class="dragulaDomClass clearfix">
+        <div class="switchingBox clearfix">
             <div class="switchingBox_title">温度传感器</div>
             <div class="wenduchuangan">58.6°C</div>
             <div class="switchIcon"></div>
@@ -42,7 +120,7 @@ function temperature() {
 
 // 复合
 function recombination() {
-    return `<div class="grid-stack-item-content">
+    return `<div class="dragulaDomClass clearfix" >
     <div class="recombination">
         <div class="recombination_t">
             <div class="recombination_t_Msg">
@@ -64,7 +142,7 @@ function recombination() {
 // 折线图html
 function HighchartsContainerSplineHtml() {
     return `
-    <div class="grid-stack-item-content">
+    <div class="dragulaDomClass clearfix" >
     <div class="lineChartBox">
         <div class="lineChartBox_title">
             1#机组温度监控
--- js/records/onlVue.js
+++ js/records/onlVue.js
@@ -1,3 +1,7 @@
+// window.onload = function(){
+
+
+
 var numbersCode = 0
 var settingDomType = true
 
@@ -20,8 +24,8 @@ new Vue({
         // 图表组件
         chartData: [{
             name: "折线图",
-            type:"lineChart",
-            show:false,
+            type: "lineChart",
+            show: false,
         }, {
             name: "柱状图",
         }, {
@@ -44,6 +48,8 @@ new Vue({
     },
     mounted() {
         this.onlGridstack()
+       
+          
     },
     methods: {
         clickHideMask() {
@@ -54,15 +60,36 @@ new Vue({
         },
         // 拖拽
         onlGridstack() {
-            var options = {
-                cellHeight: 10,
-                verticalMargin: 10,
-                float: true,
-                always_show_resize_handle: false,
-                disableResize: false
-            };
-            console.log('1')
-            $('.grid-stack').gridstack(options);
+            // var options = {
+            //     cellHeight: 10,
+            //     verticalMargin: 10,
+            //     float: true,
+            //     always_show_resize_handle: false,
+            //     disableResize: false
+            // };
+            // console.log('1')
+            // $('.grid-stack').gridstack(options);
+            dragula([document.getElementById('dragulaDom')], {
+                isContainer: function (el) {
+                    return false; // only elements in drake.containers will be taken into account
+                },
+                moves: function (el, source, handle, sibling) {
+                    return true; // elements are always draggable by default
+                },
+                accepts: function (el, target, source, sibling) {
+                    return true; // elements can be dropped in any of the `containers` by default
+                },
+                invalid: function (el, handle) {
+                    return false; // don't prevent any drags from initiating by default
+                },
+                direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
+                copy: false,                       // elements are moved by default, not copied
+                copySortSource: false,             // elements in copy-source containers can be reordered
+                revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
+                removeOnSpill: true,              // spilling will `.remove` the element, if this is true
+                mirrorContainer: document.body,    // set the element that gets mirror elements appended
+                ignoreInputTextSelection: true     // allows users to select input text, see details below
+            });
         },
         // 添加组件事件
         clickPushCom(type) {
@@ -70,72 +97,51 @@ new Vue({
             var newNameId = 'swidch' + numbersCode++
 
             if (type === 'switching') {
-                new function () {
-                    this.grid = $('.grid-stack').data('gridstack');
-                    this.add_new_widget = function () {
-                        var node = {
-                            x: 12 * Math.random(),
-                            y: 5 * Math.random(),
-                            width: 4,
-                            height: 8
-                        };
-                        this.grid.add_widget($(`<div id="" class="grid-stack-item"  data-gs-x="0" data-gs-y="0" data-gs-width="4" data-gs-height="8">${switchId(newNameId)}</div>`),
-                            node.x, node.y, node.width, node.height);
-                    }.bind(this);
-                    this.add_new_widget()
-                    that.jqClickSwitch(newNameId)
+                $('#dragulaDom').append(switchId(newNameId))
+                layui.use('colorpicker', function(){
+                    var colorpicker = layui.colorpicker;
+                    //渲染
+                    colorpicker.render({
+                        elem: '#switchIconSetting_options_body_color_elementId'
+                        ,color: '#c71585'
+                        ,predefine: true // 开启预定义颜色
+                      });
+                  }); 
+                layui.use('form',function(){
+                    var form = layui.form;
+                    form.render()
+                });
 
-                };
+                that.jqClickSwitch(newNameId)
+
             } else if (type === 'number') {
-                new function () {
-                    this.grid = $('.grid-stack').data('gridstack');
-                    this.add_new_widget = function () {
-                        var node = {
-                            x: 11 * Math.random(),
-                            y: 5 * Math.random(),
-                            width: 4,
-                            height: 8
-                        };
-                        this.grid.add_widget($(`<div " class="grid-stack-item">${temperature()}</div>`),
-                            node.x, node.y, node.width, node.height);
-                    }.bind(this);
-                    this.add_new_widget()
-                    // that.jqClickSwitch(newNameId)
+                $('#dragulaDom').append(temperature())
+                // that.jqClickSwitch(newNameId)
+            } else if (type === 'recombination') {
 
-                };
-            }else if(type === 'recombination'){
-                var options = {
-                    width:4,
-                    height:13,
-                    template:recombination()
-                }
-                this.newthisAddGrig(options)
+                $('#dragulaDom').append(recombination())
+
             }
         },
         // 图表类增加
-        clickChart(type,index){
+        clickChart(type, index) {
             console.log(type)
-            if(type ==='lineChart'){
+            if (type === 'lineChart') {
                 var show = this.chartData[index].show
                 console.log(show)
-                if(!show){
-                    console.log('还是满足？')
-                    var options = {
-                        width:7,
-                        height:22,
-                        template:HighchartsContainerSplineHtml()
-                    }
-                    this.newthisAddGrig(options).then(()=>{
-                            HighchartsContainerSpline()
-                    })
+                if (!show) {
+                    $('#dragulaDom').append(HighchartsContainerSplineHtml())
+                    HighchartsContainerSpline()
+
+
                     this.chartData[index].show = true
-                }else{
+                } else {
                     layer.open({
                         title: '已经创建'
-                        ,content: '你已经创建过折线图'
-                      });   
+                        , content: '你已经创建过折线图'
+                    });
                 }
-                
+
             }
         },
         jqClickSwitch(newNameId) {
@@ -157,38 +163,53 @@ new Vue({
 
             // 打开设置
             var settingDom = `body #${newNameId} .switchIconSetting`
-            $(settingDom).on('click',function(){
-                    console.log(settingDomType)
-                if(settingDomType){
+            $(settingDom).on('click', function () {
+                console.log(settingDomType)
+                if (settingDomType) {
                     $(this).children('.switchIconSetting_tab').show()
                     settingDomType = false
-                }else{
+                } else {
                     $(this).children('.switchIconSetting_tab').hide()
                     settingDomType = true
                 }
             })
 
+            // 编辑设置
+
+            $(`body #${newNameId}  #edit`).on('click',function(event){
+                event.stopPropagation()
+
+                $(`body #${newNameId}  #switchIconSetting_optionsDom`).show()
+            })
+            $(`body #${newNameId}  #switchIconSetting_optionsDom`).on('click',function(event){
+                event.stopPropagation()
+            })
+            $(`body #${newNameId}  .switchIconSetting_options_nav_cler`).on('click',function(){
+                $(`body #${newNameId}  #switchIconSetting_optionsDom`).hide()
+
+            })
         },
         //把PUSH到拖拽的方法封装
-        newthisAddGrig(options){
-            return new Promise((resove,reject)=>{
-                new function () {
-                    this.grid = $('.grid-stack').data('gridstack');
-                    this.add_new_widget = function () {
-                        var node = {
-                            x: 11 * Math.random(),
-                            y: 5 * Math.random(),
-                            width: options.width,
-                            height:options.height
-                        };
-                        this.grid.add_widget($(`<div " class="grid-stack-item"   data-gs-width="4" data-gs-height="8">${options.template}</div>`),
-                            node.x, node.y, node.width, node.height);
-                    }.bind(this);
-                    this.add_new_widget()
-                    resove()
-                };
-            })
-        
-        }
+        // newthisAddGrig(options){
+        //     return new Promise((resove,reject)=>{
+        //         new function () {
+        //             this.grid = $('.grid-stack').data('gridstack');
+        //             this.add_new_widget = function () {
+        //                 var node = {
+        //                     x: 11 * Math.random(),
+        //                     y: 5 * Math.random(),
+        //                     width: options.width,
+        //                     height:options.height
+        //                 };
+        //                 this.grid.add_widget($(`<div " class="grid-stack-item"   data-gs-width="4" data-gs-height="8">${options.template}</div>`),
+        //                     node.x, node.y, node.width, node.height);
+        //             }.bind(this);
+        //             this.add_new_widget()
+        //             resove()
+        //         };
+        //     })
+
+        // }
     },
 })
+// }
